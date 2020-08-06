dfe073681572625d0e9cac8b7a1e5e0cc2c0b168
Pierre-Olivier Trottier <po.trottier@gmail.com>
2020-08-06 09:54:52 -0400

    GitIgnore

--- node_modules/@azure/service-bus/src/util/parseUrl.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-const url = require("url");
-
-export const parseURL = (rawUrl: string) => {
-  return new url.URL(rawUrl);
-};
