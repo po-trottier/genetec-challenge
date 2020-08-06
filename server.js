986aa8f481232112925bac33efb1a5b6715b3f73
Pierre-Olivier Trottier <po.trottier@gmail.com>
2020-08-06 11:20:52 -0400

    Refactor

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 const { ServiceBusClient, ReceiveMode } = require("@azure/service-bus"); 
 const axios = require('axios');
+const fs = require('fs');
 
 // Define connection string and related Service Bus entity names here
 const endpoint = "Endpoint=sb://licenseplatepublisher.servicebus.windows.net/;SharedAccessKeyName=ConsumeReads;SharedAccessKey=VNcJZVQAVMazTAfrssP6Irzlg/pKwbwfnOqMXqROtCQ=";
 const topicName = "licenseplateread"; 
 const subscriptionKey = "lljogbgtkpoozqvj"; 
 
-async function main(){
+async function main() {
   // SETUP
   const sbClient = ServiceBusClient.createFromConnectionString(endpoint); 
   const subscriptionClient = sbClient.createSubscriptionClient(topicName, subscriptionKey);
   const receiver = subscriptionClient.createReceiver(ReceiveMode.receiveAndDelete);
 
   try {
-    const messages = await receiver.receiveMessages(1);
-
+    // GET MESSAGES
+    const messages = await receiver.receiveMessages(50);
     const plates = messages.map(message => message.body);
 
-    for(const plate of plates) {
+    // GET LOCAL WANTED PLATES
+    const wanted = JSON.parse(fs.readFileSync('./data/wanted.json'));
+    console.log(wanted);
+
+    // FILTER FOUND PLATES WITH WANTED PLATES
+    const filtered = plates.filter(plate => wanted.includes(plate.LicensePlate));
+    console.log(filtered.map(item => item.LicensePlate));
+
+    // SEND THE WANTED PLATES
+    for(const plate of filtered) {
       let payload = {
         LicensePlateCaptureTime : plate.LicensePlateCaptureTime,
         LicensePlate            : plate.LicensePlate,
@@ -25,7 +35,7 @@ async function main(){
         Longitude               : plate.Longitude
       }
       console.log(payload);
-
+    
       // SEND THE REQUEST
       const response = await axios.post(
         'https://licenseplatevalidator.azurewebsites.net/api/lpr/platelocation', 
@@ -36,29 +46,26 @@ async function main(){
           }
         }
       );
-      console.log(`Sent plate data to platform, request status: ${response.status} (${response.statusText})`);
-
-      // GET WANTED PLATES
-      // THIS COSTS MONEY !!!
-      // DATA IN WANTED.JSON
-      // let plates = await axios.get('https://licenseplatevalidator.azurewebsites.net/api/lpr/wantedplates', {
-      //   auth: {
-      //     username: 'team02',
-      //     password: ']))XiyRbLKT=)ds!'
-      //   }
-      // });
-      // plates = plates.data;
-      // console.log(plates);
-
-      // FILTER FOUND PLATES WITH WANTED PLATES
-      // const filtered = array.filter(item => plates.includes(item.LicensePlate))
-      // console.log(filtered);
+      console.log(response.data);
     }
 
     await subscriptionClient.close();
   } finally {
     await sbClient.close();
   }
+}
+
+async function getWanted() {
+  // GET WANTED PLATES
+  // THIS COSTS MONEY !!!
+  // DATA IN WANTED.JSON
+  const plates = await axios.get('https://licenseplatevalidator.azurewebsites.net/api/lpr/wantedplates', {
+    auth: {
+      username: 'team02',
+      password: ']))XiyRbLKT=)ds!'
+    }
+  });
+  fs.writeFileSync('./data/wanted.json', plates.data);
 }
 
 main().catch((err) => {
