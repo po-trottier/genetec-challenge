9ca04b86a89c979865d7cd018e4ddc0ab95fd446
Pierre-Olivier Trottier <po.trottier@gmail.com>
2020-08-06 11:33:35 -0400

    Full refator

--- server.js
+++ server.js
@@ -2,64 +2,69 @@ const { ServiceBusClient, ReceiveMode } = require("@azure/service-bus");
 const axios = require('axios');
 const fs = require('fs');
 
-// Define connection string and related Service Bus entity names here
-const endpoint = "Endpoint=sb://licenseplatepublisher.servicebus.windows.net/;SharedAccessKeyName=ConsumeReads;SharedAccessKey=VNcJZVQAVMazTAfrssP6Irzlg/pKwbwfnOqMXqROtCQ=";
-const topicName = "licenseplateread"; 
-const subscriptionKey = "lljogbgtkpoozqvj"; 
+// CONSTANTS
+const platesEndpoint = "Endpoint=sb://licenseplatepublisher.servicebus.windows.net/;SharedAccessKeyName=ConsumeReads;SharedAccessKey=VNcJZVQAVMazTAfrssP6Irzlg/pKwbwfnOqMXqROtCQ=";
+const platesTopicName = "licenseplateread"; 
+const platesSubscriptionKey = "lljogbgtkpoozqvj"; 
+const wantedEndpoint = "Endpoint=sb://licenseplatepublisher.servicebus.windows.net/;SharedAccessKeyName=listeneronly;SharedAccessKey=w+ifeMSBq1AQkedLCpMa8ut5c6bJzJxqHuX9Jx2XGOk=";
+const wantedTopicName = "wantedplatelistupdate"; 
+const wantedSubscriptionKey = "lljogbgtkpoozqvj"; 
 
 async function main() {
   // SETUP
-  const sbClient = ServiceBusClient.createFromConnectionString(endpoint); 
-  const subscriptionClient = sbClient.createSubscriptionClient(topicName, subscriptionKey);
-  const receiver = subscriptionClient.createReceiver(ReceiveMode.receiveAndDelete);
+  const platesClient = ServiceBusClient.createFromConnectionString(platesEndpoint); 
+  const platesSubscription = platesClient.createSubscriptionClient(platesTopicName, platesSubscriptionKey);
+  const platesReceiver = platesSubscription.createReceiver(ReceiveMode.receiveAndDelete);
+
+  const wantedClient = ServiceBusClient.createFromConnectionString(wantedEndpoint); 
+  const wantedSubscription = wantedClient.createSubscriptionClient(wantedTopicName, wantedSubscriptionKey);
+  const wantedReceiver = wantedSubscription.createReceiver(ReceiveMode.receiveAndDelete);
 
   try {
-    // GET MESSAGES
-    const messages = await receiver.receiveMessages(50);
-    const plates = messages.map(message => message.body);
-
-    sendLicensePlates(plates);
-    
-    await subscriptionClient.close();
+    platesReceiver.registerMessageHandler(sendLicensePlates, onError);    
+    wantedReceiver.registerMessageHandler(getWanted, onError);
+    await platesSubscription.close();
+    await wantedSubscription.close();
   } finally {
-    await sbClient.close();
+    await platesClient.close();
+    await wantedClient.close();
   }
 }
 
-async function sendLicensePlates(plates) {
+async function sendLicensePlates(message) {
+  const plate = message.body;
+
   // GET LOCAL WANTED PLATES
   const wanted = JSON.parse(fs.readFileSync('./data/wanted.json'));
   console.log(wanted);
 
   // FILTER FOUND PLATES WITH WANTED PLATES
-  const filtered = plates.filter(plate => wanted.includes(plate.LicensePlate));
-  console.log(filtered.map(item => item.LicensePlate));
+  if(!wanted.includes(plate.LicensePlate))
+    return;
 
   // SEND THE WANTED PLATES
-  for(const plate of filtered) {
-    let payload = {
-      LicensePlateCaptureTime : plate.LicensePlateCaptureTime,
-      LicensePlate            : plate.LicensePlate,
-      Latitude                : plate.Latitude,
-      Longitude               : plate.Longitude
-    }
-    console.log(payload);
-  
-    // SEND THE REQUEST
-    const response = await axios.post(
-      'https://licenseplatevalidator.azurewebsites.net/api/lpr/platelocation', 
-      payload,
-      {
-        headers: {
-          Authorization: 'Basic dGVhbTAyOl0pKVhpeVJiTEtUPSlkcyE=' 
-        }
-      }
-    );
-    console.log(response.data);
+  let payload = {
+    LicensePlateCaptureTime : plate.LicensePlateCaptureTime,
+    LicensePlate            : plate.LicensePlate,
+    Latitude                : plate.Latitude,
+    Longitude               : plate.Longitude
   }
+  console.log(payload);
+  
+  // SEND THE REQUEST
+  const response = await axios.post(
+    'https://licenseplatevalidator.azurewebsites.net/api/lpr/platelocation', 
+    payload,
+    {
+      headers: {
+        Authorization: 'Basic dGVhbTAyOl0pKVhpeVJiTEtUPSlkcyE=' 
+      }
+    }
+  );
+  console.log(response.data);
 }
 
-async function getWanted() {
+async function getWanted(message) {
   // GET WANTED PLATES
   // THIS COSTS MONEY !!!
   // DATA IN WANTED.JSON
@@ -72,6 +77,11 @@ async function getWanted() {
   fs.writeFileSync('./data/wanted.json', plates.data);
 }
 
+async function onError(error) {
+  console.error(error);
+}
+
+// RUN
 main().catch((err) => {
   console.log("Error occurred: ", err);
 });
