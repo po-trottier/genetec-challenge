848b8a0950c5aad79ace3889fd9f3039d079d8ab
Pierre-Olivier Trottier <po.trottier@gmail.com>
2020-08-06 10:21:14 -0400

    Update

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const { ServiceBusClient, ReceiveMode } = require("@azure/service-bus"); 
 const axios = require('axios');
-const fs = require('fs');
 
 // Define connection string and related Service Bus entity names here
 const endpoint = "Endpoint=sb://licenseplatepublisher.servicebus.windows.net/;SharedAccessKeyName=ConsumeReads;SharedAccessKey=VNcJZVQAVMazTAfrssP6Irzlg/pKwbwfnOqMXqROtCQ=";
@@ -13,9 +12,34 @@ async function main(){
   const receiver = subscriptionClient.createReceiver(ReceiveMode.receiveAndDelete);
 
   try {
-    const messages = await receiver.receiveMessages(10);
-    
-    fs.writeFileSync('./data/response.json', messages.map(message => JSON.stringify(message.body)));
+    const messages = await receiver.receiveMessages(1);
+  
+    const array = messages.map(message => JSON.stringify(message.body));
+
+    for (let i = 0; i < array.length; i++) {
+      const obj = JSON.parse(array[i]);
+      const body = {
+        LicensePlateCaptureTime: obj.LicensePlateCaptureTime,
+        LicensePlate: obj.LicensePlate,
+        Latitude: obj.Latitude,
+        Longitude: obj.Longitude,
+      };
+
+      console.log(body);
+
+      // const response = await axios.post(
+      //   'https://licenseplatevalidator.azurewebsites.net/api/lpr/platelocation', 
+      //   body,
+      //   {
+      //     auth: {
+      //       username: 'team02',
+      //       password: ']))XiyRbLKT=)ds!'
+      //     }
+      //   }
+      // );
+
+      // console.log(response);
+    }
 
     await subscriptionClient.close();
   } finally {
